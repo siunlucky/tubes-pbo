02d80059712e943d398563b93a773e6fa708798a
muamarzidan <codeofomiru11@gmail.com>
2025-05-15 21:03:59 +0700

    test

--- src/main/java/com/example/tubes/controller/UserController.java
+++ src/main/java/com/example/tubes/controller/UserController.java
@@ -24,10 +24,10 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    // @GetMapping
-    // public String getAllUsers() {
-    //     return "test";
-    // }
+    public void test()
+    {
+        System.out.println("test");
+    }    
     
     @GetMapping
     public List<User> getAllUsers() {
